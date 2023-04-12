da0cecd866456471d3eda7c89ccd92674d5eb9a1
Ankit Singh <ankit.bitmsra@gmail.com>
2023-04-12 01:14:33 -0400

    Update setup_cuda.py

--- setup_cuda.py
+++ setup_cuda.py
@@ -5,7 +5,7 @@ setup(
     name='quant_cuda',
     ext_modules=[cpp_extension.CUDAExtension(
         'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu'],
-        nvcc_args=['-arch=sm_61']
+        extra_compile_args={'nvcc': ['-arch=sm_61']},
     )],
     cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
